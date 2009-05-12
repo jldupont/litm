497966537a0add25084f7e58b79e94261ced01f2
jeanlou.dupont <jeanlou.dupont@60bcbc50-30cf-11de-8e0c-53a27eea117e>
2009-05-12 14:25:49 +0000

    release 0.1

---
+++ branches/0.1/project/includes/queue.h
@@ -0,0 +1,30 @@
+/**
+ * @file   queue.h
+ *
+ * @date   2009-04-23
+ * @author Jean-Lou Dupont
+ */
+
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "litm.h"
+
+
+	// Prototypes
+	// ==========
+	queue *queue_create(void);
+	void   queue_destroy(queue *queue);
+
+	int   queue_put_nb(queue *q, void *msg);
+	int   queue_put(queue *q, void *msg);
+	void *queue_get(queue *q);
+	void *queue_get_nb(queue *q);
+	int   queue_peek(queue *q);
+
+	int queue_put_safe( queue *q, void *node );
+
+#endif /* QUEUE_H_ */
---
+++ branches/0.1/project/src/logger.c
@@ -0,0 +1,33 @@
+/*
+ * logger.c
+ *
+ *  Created on: 2009-04-21
+ *      Author: Jean-Lou Dupont
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "logger.h"
+
+char *_LOGGER_IDENTITY = "litm";
+
+/**
+ * Crude logging function
+ */
+void doLog(int priority, char *message, ...) {
+
+	openlog(_LOGGER_IDENTITY, LOG_PID, LOG_LOCAL1);
+
+	//char buffer[2048];
+	va_list ap;
+
+	va_start(ap, message);
+		vsyslog( priority, message, ap);
+		//vsnprintf (buffer, sizeof(buffer), message, ap);
+	va_end(ap);
+
+	//syslog(priority, buffer, NULL);
+
+	closelog();
+
+}
